944e5d5417504188f041a182be589961e3220df6
Anthony Danger <anthony.sanchez.sf@gmail.com>
2013-04-11 14:44:50 -0700

    Add Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Omrails::Application.configure do
 
   # In production :host should be set to the actual host of your application
     config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+    
   # Configuring Amazon S3 for paperclip file uploads
   config.paperclip_defaults = {
     :storage => :s3,
